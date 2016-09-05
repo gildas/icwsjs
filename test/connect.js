b153e947400aa66ecd00600589bb71ca66ce4cbe
Gildas Cherruel <gildas.cherruel@inin.com>
2016-09-05 23:44:27 +0900

    connection ok

--- test/connect.js
+++ test/connect.js
@@ -56,8 +56,12 @@ describe('Session', function() {
     it('should connect with valid credentials', function() {
       return icws.connect(config.url, config.users.default.user, config.users.default.password)
       .then(function(data){
-        expect(data).to.have.property('id');
-        expect(data.id).to.be.ok();
+        expect(data).to.have.property('id').to.be.ok;
+        expect(data).to.have.property('token').to.be.ok;
+        expect(data).to.have.property('cookie').to.be.ok;
+        expect(data).to.have.property('ic_server').to.be.ok;
+        expect(data).to.have.property('alternates').to.be.ok;
+        expect(data).to.have.property('user_id').to.be.ok;
       });
     });
   });
