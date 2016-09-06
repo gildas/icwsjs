df5ef9e14980a73b9e2c688d4ecee7230a70301c
Cherruel, Gildas <gildas.cherruel@inin.com>
2016-09-06 15:57:59 +0900

    Disconnect works

--- test/connect.js
+++ test/connect.js
@@ -11,23 +11,20 @@ const config = {
   url: "https://localhost:8019",
   users: {
     default:    { user: 'agent',        password: '1234' },
-    no_license: { user: 'agent_nolic',  password: '1234' }.
-    no_locked:  { user: 'agent_locked', password: '1234' }.
+    expired:    { user: 'agent_locked', password: '1234' },
   }
 };
 
 describe('Session', function() {
   var icws = new ICWS.Session();
 
-  beforeEach(function() { icws.disconnect(); });
-
   describe('#connect', function() {
     it('should not connect with wrong protocol', function() {
-      return expect(icws.connect('tcp://cic.acme.org', 'agent', '1234')).to.be.rejectedWith('com.inin.icws.error.url.invalid_protocol(tcp:)');
+      return expect(icws.connect('tcp://cic.acme.org', 'agent', '1234')).to.be.rejectedWith('com.inin.icws.error.url.invalid_protocol');
     });
 
     it('should not connect with wrong port', function() {
-      return expect(icws.connect('https://cic.acme.org:443', 'agent', '1234')).to.be.rejectedWith('com.inin.icws.error.url.invalid_port(443)');
+      return expect(icws.connect('https://cic.acme.org:443', 'agent', '1234')).to.be.rejectedWith('com.inin.icws.error.url.invalid_port');
     });
 
     it('should not connect with empty user', function() {
@@ -39,20 +36,16 @@ describe('Session', function() {
     });
 
     it('should not connect with wrong password', function() {
-      //return expect(icws.connect(config.url, config.users.default.user, config.users.default.password + 'wrong')).to.be.rejectedWith('com.inin.icws.error.connect.invalid_password');
+      return expect(icws.connect(config.url, config.users.default.user, config.users.default.password + 'wrong')).to.be.rejectedWith('com.inin.icws.error.connect.invalid_credentials');
     });
 
     it('should not connect with invalid user', function() {
-      //return expect(icws.connect(config.url, config.users.default.user + 'notexist', config.users.default.password)).to.be.rejectedWith('com.inin.icws.error.connect.invalid_user');
+      return expect(icws.connect(config.url, config.users.default.user + 'notexist', config.users.default.password)).to.be.rejectedWith('com.inin.icws.error.connect.invalid_credentials');
     });
 
-    // pending tests...
     it('should not connect with expired password', function() {
+      return expect(icws.connect(config.url, config.users.expired.user, config.users.expired.password)).to.be.rejectedWith('com.inin.icws.error.connect.invalid_credentials');
     });
-
-    it('should not connect with unlicensed user', function() {
-    });
-    // end of pending tests...
 
     it('should connect with valid credentials', function() {
       return icws.connect(config.url, config.users.default.user, config.users.default.password)
@@ -69,17 +62,9 @@ describe('Session', function() {
 
   describe('#disconnect', function() {
     it('should disconnect a connected session', function() {
-      return icws.connect(config.url, config.users.default.user, config.users.default.password)
+      return icws.disconnect()
       .then(function(data) {
-        expect(data).to.have.property('id').to.be.ok;
-        expect(data).to.have.property('token').to.be.ok;
-        expect(data).to.have.property('cookie').to.be.ok;
-        expect(data).to.have.property('ic_server').to.be.ok;
-        expect(data).to.have.property('alternates').to.be.ok;
-        expect(data).to.have.property('user_id').to.be.ok;
-      })
-      .then(function(data) {
-        return icws.disconnect();
+        expect(data.is_connected).to.be.false;
       });
     });
   });
